6cf016f927cdd692410353c5e4fe7138274dff17
Clifton Tam <ctam@live.ca>
2016-03-26 22:37:43 -0700

    fixed bugs

--- application/models/Timetable.php
+++ application/models/Timetable.php
@@ -16,45 +16,45 @@ class Timetable extends CI_Model {
 
         $this->xml = simplexml_load_string(file_get_contents('data/timetable.xml'));
         
-        foreach ($this->xml->periods->block as $period) {
-            foreach ($period->class as $class) {
+        foreach ($this->xml->periods->block as $block) {
+            foreach ($block->class as $class) {
                 $temp = array();
-                $temp['time'] = (string) $class['time'];
+                $temp['time'] = (string) $block['time'];
                 $temp['day'] = (string) $class['day'];
                 $temp['name'] = (string) $class->name;
                 $temp['code'] = (string) $class->code;
                 $temp['classinstructor'] = (string) $class->classinstructor;
                 $temp['location'] = (string) $class->location;
                 $temp['type'] = (string) $class->type;
-                $this->periods[] = new Booking($temp);
+                $this->blocks[] = new Booking($temp);
             }
         }
         
         foreach ($this->xml->instructors->instructor as $instructor) {
             foreach ($instructor->class as $class) {
                 $temp = array();
-                $temp['name'] = (string) $class['name'];
+                $temp['classinstructor'] = (string) $instructor['name'];
                 $temp['time'] = (string) $class['time'];  
                 $temp['name'] = (string) $class->name;
                 $temp['code'] = (string) $class->code;
-                $temp['dayofclass'] = (string) $class->dayofclass;
+                $temp['day'] = (string) $class->dayofclass;
                 $temp['location'] = (string) $class->location;
                 $temp['type'] = (string) $class->type;
-                $this->periods[] = new Booking($temp);
+                $this->instructors[] = new Booking($temp);
             }
         }
         
         foreach ($this->xml->daysofweek->day as $day) {
             foreach ($day->class as $class) {
                 $temp = array();
-                $temp['day'] = (string) $class['day'];
+                $temp['day'] = (string) $day['name'];
                 $temp['time'] = (string) $class['time'];  
                 $temp['name'] = (string) $class->name;
                 $temp['code'] = (string) $class->code;
                 $temp['classinstructor'] = (string) $class->classinstructor;
                 $temp['location'] = (string) $class->location;
                 $temp['type'] = (string) $class->type;
-                $this->periods[] = new Booking($temp);
+                $this->daysofweek[] = new Booking($temp);
             }
         }
     }
@@ -77,17 +77,22 @@ class Timetable extends CI_Model {
 class Booking extends CI_Model {
     public $name;
     public $code;
+    public $time;
     public $dayofclass;
     public $classinstructor;
     public $location;
     public $type;
-
+    
     public function __construct($booking) {
-       $this->$name = (string) $booking['$name'];
-       $this->$code = (string) $booking['$code'];
-       $this->$dayofclass = (string) $booking['$dayofclass'];
-       $this->$classinstructor = (string) $booking['$classinstructor'];
-       $this->$type = (string) $booking['$type']; 
+        
+       $this->time = (string) $booking['time'];
+       $this->name = (string) $booking['name'];
+       $this->code = (string) $booking['code'];
+       $this->time = (string) $booking['time'];
+       $this->dayofclass = (string) $booking['day'];
+       $this->classinstructor = (string) $booking['classinstructor'];
+       $this->location = (string) $booking['location'];
+       $this->type = (string) $booking['type']; 
     }
     
 }
